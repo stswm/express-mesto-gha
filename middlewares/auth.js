c15d912f122a754977f91cf21e8e6bccaaef199e
Oleg Azadov <oazadov@gmail.com>
2022-06-07 15:55:13 +0300

    1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,14 @@ const { AuthError } = require('../Errors/AuthError');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthError('Необходима авторизация1');
+    throw new AuthError('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'testKey');
   } catch (err) {
-    throw new AuthError('Необходима авторизация2');
+    throw new AuthError('Необходима авторизация');
   }
   req.user = payload;
   next();
